1b7ae2da94affd4ccae92e5a8d2666e9f1540053
BassemArfaoui <bassem.arfaoui@insat.ucar.tn>
2024-12-09 19:16:01 +0100

    add ac route

--- api/ac.js
+++ api/ac.js
@@ -3,38 +3,39 @@ const mqtt = require('mqtt');
 const brokerUrl = 'mqtts://db22137176fc49dbb309bff1972cb054.s1.eu.hivemq.cloud';
 const mqttClient = mqtt.connect(brokerUrl, {
     port: 8883,
-    username: 'bassem',
-    password: 'bassemMQTT123',
+    username: 'bassem', 
+    password: 'bassemMQTT123', 
 });
 
-const mqttTopic = 'heating/test';
-
-// Connect to the MQTT broker and subscribe to the topic
 mqttClient.on('connect', () => {
     console.log('Connected to HiveMQ Cloud MQTT broker');
-    mqttClient.subscribe(mqttTopic, (err) => {
-        if (err) {
-            console.error('Failed to subscribe to topic:', err);
-        } else {
-            console.log(`Subscribed to topic: ${mqttTopic}`);
-        }
-    });
 });
 
-
-// Handle MQTT errors
 mqttClient.on('error', (err) => {
     console.error('MQTT connection error:', err);
 });
 
-// API handler function
-export default function handler(req, res) {
-    if (req.method === 'GET') {
-        // Return the latest message as-is
-       res.status(200).json({
-            message: 'test',
-        });
-    } else {
-        res.status(405).json({ message: 'Method not allowed' });
+module.exports = (req, res) => {
+    const { temp } = req.query;
+
+    if (!temp) {
+        return res.status(400).json({ error: 'temp parameter is required' });
     }
-}
+
+
+    if (temp < 16 || temp >30 ) {
+        return res.status(400).json({ error: `temperature must be between 16 and 30` });
+    }
+
+    const topic = 'heating-system/ac';
+
+    mqttClient.publish(topic, mode.toUpperCase(), (err) => {
+        if (err) {
+            console.error('Publish error:', err);
+            return res.status(500).json({ error: 'Failed to publish mode' });
+        }
+
+        console.log(`Temp ${temp} published to topic ${topic}`);
+        return res.status(200).json({ success: true, temp });
+    });
+};
